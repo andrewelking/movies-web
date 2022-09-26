25f0692bbe1867eb1e961163f441ff8749eecc6c
Minh Tân <athousandblossom@gmail.com>
2022-09-26 16:05:46 +0700

    added and configed routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import MovieRoutes from './config/MovieRoutes';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/*' element={<MovieRoutes />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/config/MovieRoutes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Routes, Route } from 'react-router-dom'
+
+import Catalog from '../pages/Catalog'
+import Detail from '../pages/Detail'
+
+const MovieRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/:category/search/:keyword' element={<Catalog />} />
+      <Route path='/:category/:id' element={<Detail />} />
+      <Route path='/:category' element={<Catalog />} />
+    </Routes>
+  );
+}
+
+export default MovieRoutes
