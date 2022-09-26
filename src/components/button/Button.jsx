e60bbdcc32efed22fe7f3ee74b46f9d0dc27ba25
Minh Tân <athousandblossom@gmail.com>
2022-09-26 19:09:48 +0700

    changed button component to propTypes

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ className, onClick }) => {
-  return <button className={`btn ${className}`} onClick={onClick ? () => onClick() : null}>
-    {children}
+const Button = props => {
+  return <button className={`btn ${props.className}`} onClick={props.onClick ? () => props.onClick() : null}>
+    {props.children}
   </button>;
 };
 
 const OutlineButton = () => {
     return (
-        <Button className={`btn-outline ${className}`} onClick={onClick ? () => onClick() : null}>
-    {children}
-  </Button>;
+        <Button className={`btn-outline ${props.className}`} onClick={props.onClick ? () => props.onClick() : null}>
+    {props.children}
+  </Button>
     )
 }
 
