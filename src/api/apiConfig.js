01ff85c8d2a9bbcfc0e904df240328fa88b26684
Minh Tân <athousandblossom@gmail.com>
2022-09-26 23:24:21 +0700

    capitalized URL

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 
 const apiConfig = {
-  baseUrl: 'https://api.themoviedb.org/3/',
+  baseURL: 'https://api.themoviedb.org/3/',
   apiKey: 'd63ad08dae016665068d99abd37b48be',
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
