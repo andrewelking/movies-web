64bafdfd361ebc94a4ea53528514b5081f6cc149
Minh Tân <athousandblossom@gmail.com>
2022-10-04 01:53:43 +0700

    added constantly changing background for page header

--- src/components/page-header/PageHeader.jsx
+++ src/components/page-header/PageHeader.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 
-import './page-header.scss'
+import './page-header.scss';
 
-import bg from '../../assets/footer-bg.jpg'
+let i = 0;
 
-const PageHeader = props => {
+const PageHeader = (props) => {
+  const [background, setBackground] = useState('');
+  const headerRef = useRef(null);
+
+  function importAll(r) {
+    let images = [];
+    r.keys().map((item, index) => {
+      return images.push(r(item));
+    });
+    return images;
+  }
+
+  const images = importAll(
+    require.context('../../assets/lorsplasharts', false, /\.(png|jpe?g|svg)$/)
+  );
+
+  useEffect(() => {
+    if (i > images.length) i = 0;
+    const interval = setInterval(() => {
+      setBackground(images[i]);
+      i++;
+      headerRef.current.setAttribute(
+        'style',
+        `background-image: url('${background}')`
+      );
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [background, images]);
+
   return (
-    <div className="page-header" style={{backgroundImage: `url${bg}`}}>
-        <h2>{props.children}</h2>
+    <div className='page-header' ref={headerRef}>
+      <h2>{props.children}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default PageHeader
+export default PageHeader;
